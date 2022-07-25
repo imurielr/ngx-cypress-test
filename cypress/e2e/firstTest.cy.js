2422262125e3fdfb7ee29a344427f9bf2c103718
imurielr <isamuri.r@gmail.com>
2022-07-25 14:35:08 -0500

    Listas y dropdowns

--- cypress/e2e/firstTest.cy.js
+++ cypress/e2e/firstTest.cy.js
@@ -156,7 +156,7 @@ describe('Our first suite', () => {
       })
   })
 
-  it.only('checkboxes', () => {
+  it('checkboxes', () => {
     cy.visit('/')
     cy.contains('Modal & Overlays').click()
     cy.contains('Toastr').click()
@@ -164,4 +164,40 @@ describe('Our first suite', () => {
     cy.get('[type="checkbox"]').check({ force: true }) // solo checkea, no se puede descheckear con .check()
     cy.get('[type="checkbox"]').eq(0).click({ force: true })
   })
+
+  it.only('lists and dropdowns', () => {
+    cy.visit('/')
+
+    // 1
+    cy.get('nav nb-select').click()
+    cy.get('.options-list').contains('Dark').click()
+    cy.get('nav nb-select').should('contain', 'Dark')
+    cy.get('nb-layout-header nav').should('have.css', 'background-color', 'rgb(34, 43, 69)')
+
+    // 2 (Click en cada opción)
+    cy.get('nav nb-select').then(dropdown => {
+      cy.wrap(dropdown).click()
+
+      cy.get('.options-list nb-option').each((listItem, index) => {
+        const itemText = listItem.text().trim()
+        
+        const colors = {
+          Light: 'rgb(255, 255, 255)',
+          Dark: 'rgb(34, 43, 69)',
+          Cosmic: 'rgb(50, 50, 89)',
+          Corporate: 'rgb(255, 255, 255)'
+        }
+        
+        cy.wrap(listItem).click()
+        cy.wrap(dropdown).should('contain', itemText)
+        cy.get('nb-layout-header nav').should('have.css', 'background-color', colors[itemText])
+        
+        if (index < 3) {
+          cy.wrap(dropdown).click()
+        }
+      })
+    })
+
+  })
+
 })
